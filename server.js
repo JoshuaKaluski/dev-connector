8271e6abd0707195737e80e2a99f1e9616cfde2c
Joshua Kaluski <joshua@kaluski.com>
2019-06-27 12:42:01 -0400

    Connected database

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+//Connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
