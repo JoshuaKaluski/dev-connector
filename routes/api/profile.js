52755394d121f760d38a215b61047e6896fc9304
Joshua Kaluski <joshua@kaluski.com>
2019-06-29 17:55:56 -0400

    Created DELETE route to delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -204,7 +204,30 @@ router.put('/experience', [auth, [
     res.json(profile);
   } catch (e) {
     console.error(e.message);
-    res.status(500).send('Server error')
+    res.status(500).send('Server error');
+  }
+});
+
+/*
+Route:       DELETE api/profile/experience/:exp_id
+Description: Delete experience from profile
+Access:      Private
+*/
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+
+    //Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile)
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send('Server error');
   }
 });
 
